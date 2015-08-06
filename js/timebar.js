da9f55016a955c6182fa45df4a9d12c1c1d9dc9e
Miron B. Kursa <M.Kursa@icm.edu.pl>
2015-08-06 19:37:49 +0200

    Time controls.

---
+++ js/timebar.js
@@ -0,0 +1,87 @@
+// Embarassingly simple timebar component
+
+var Timebar=function(s0,play0,mar,height,bottom){
+ var k=document.createElement('canvas');
+ k.style.position='fixed';
+ k.style.bottom=bottom;
+ k.style.left='0px';
+ k.width=window.innerWidth;
+ k.height=height;
+ document.body.appendChild(k);
+
+ var c=k.getContext('2d');
+ this.s=s0;
+ this.play=play0;
+ var myself=this;
+
+ function paint(){
+  c.clearRect(0,0,k.width,k.height);
+
+  c.strokeStyle="#000";
+  c.strokeWidth=20;
+  c.fillStyle="#ccc";
+  c.fillRect(mar*3,0,(k.width-4*mar),k.height);
+
+  if(myself.play) c.fillStyle="#a00";
+  c.beginPath();
+  c.moveTo(mar,0.1*k.height);
+  c.lineTo(2.7*mar,k.height/2);
+  c.lineTo(mar,0.9*k.height);
+  c.lineTo(mar,0.1*k.height);
+  c.fill();
+  c.stroke();
+
+  c.fillStyle="#a00";
+  c.fillRect(mar*3,0,(k.width-4*mar)*myself.s,k.height);
+  c.strokeRect(mar*3,0,(k.width-4*mar),k.height);
+ }
+ this.repaint=paint;
+
+ this.setS=function(newS){
+  this.s=newS;
+  if(this.s<0.) this.s=0.;
+  if(this.s>1.) this.s=1.;
+  paint();
+ }
+
+ function resizeHandler(ev){
+  k.width=window.innerWidth;
+  paint();
+ }
+ function moveHandler(ev){
+  myself.s=(ev.clientX-mar*3)/(k.width-4*mar);
+  if(myself.s<0.) myself.s=0.;
+  if(myself.s>1.) myself.s=1.;
+  paint();
+  ev.stopPropagation();
+ }
+ var restorePlay=this.play;
+ function downHandler(ev){
+  if(ev.clientX<mar*3){
+   //Toggle playing
+   myself.play=!myself.play;
+   paint();
+  }else{
+   //Drag
+   window.addEventListener('mousemove',moveHandler,false);
+   moveHandler(ev);
+   window.addEventListener('mouseup',upHandler,false);
+   //Disable playing for the time of drag
+   restorePlay=myself.play;
+   myself.play=false;
+  }
+  ev.stopPropagation();
+ }
+
+ function upHandler(ev){
+  window.removeEventListener('mousemove',moveHandler,false);
+  window.removeEventListener('mouseup',upHandler,false);
+  myself.play=restorePlay;
+  paint();
+  ev.stopPropagation();
+ }
+
+ k.addEventListener('mousedown',downHandler,false);
+ window.addEventListener('resize',resizeHandler,false);
+ resizeHandler();
+}
