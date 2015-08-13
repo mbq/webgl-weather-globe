0e3ac5dff66c02cdddd4172dc68994af26b5ab2a
Miron B. Kursa <M.Kursa@icm.edu.pl>
2015-08-13 21:46:23 +0200

    Github page.

--- download-data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget -nc http://140.172.38.100/psd/thredds/fileServer/Datasets/ncep/uwnd.2014.nc
-wget -nc http://140.172.38.100/psd/thredds/fileServer/Datasets/ncep/vwnd.2014.nc
-wget -nc http://140.172.38.100/psd/thredds/fileServer/Datasets/ncep/air.2014.nc
